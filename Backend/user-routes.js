144fd6c0346e6bbfa596c35dd17e63918c8618e8
alvo <alvin-alvo@users.noreply.github.com>
2025-01-11 15:00:28 +0530

    new change

--- Backend/user-routes.js
+++ Backend/user-routes.js
@@ -8,7 +8,16 @@ router.get('/', (req, res) => {
 
 router.post('/issue', async (req, res) => {
     try {
-
+        const { certificateID, courseName, candidateName, grade, date } = req.body;
+        console.log(certificateID);
+        if (certDetails.has(certificateID)) {
+            res.status(201).json({ message: `${certificateID} already exist` })
+        }
+        else {
+            certDetails.set(certificateID, { courseName, candidateName, grade, date });
+            console.log(certDetails.get(certificateID));
+            res.status(201).json({ message: 'Certificate details saved' })
+        }
 
         
     } catch (error) {
@@ -18,7 +27,16 @@ router.post('/issue', async (req, res) => {
 })
 
 router.get('/view/:id', async (req, res) => {
-    
+    const id1 = req.params.id;
+    console.log(id1);
+    const details = certDetails.get(id1);
+    console.log(details);
+    if (details) {
+        res.json(details);
+    }
+    else {
+        res.status(404).json({ message: 'details not found' });
+    }
 
 })
 
